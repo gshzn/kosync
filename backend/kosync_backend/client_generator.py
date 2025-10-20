8c471583499cc7dacc7a18035fffaf6c587259f5
Guus Huizen <guus@guus.tech>
2025-10-20 14:36:29 +0200

    Finish client generation

--- backend/kosync_backend/client_generator.py
+++ backend/kosync_backend/client_generator.py
@@ -1,7 +1,7 @@
 import json
 import shutil
 import tarfile
-from tempfile import TemporaryDirectory, tempdir
+from tempfile import TemporaryDirectory
 from typing import Self
 from urllib.request import urlretrieve
 from pathlib import Path
@@ -20,12 +20,14 @@ class ClientGenerator:
         self.root_directory = self._prepare_nickeldbus()
         self._prepare_client()
         return self
-    
+
     def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
         shutil.rmtree(self.root_directory)
 
     def _download_latest_nickelmenu_version(self) -> Path:
-        path, _ = urlretrieve("https://github.com/pgaskin/NickelMenu/releases/download/v0.5.4/KoboRoot.tgz")
+        path, _ = urlretrieve(
+            "https://github.com/pgaskin/NickelMenu/releases/download/v0.5.4/KoboRoot.tgz"
+        )
 
         return Path(path)
 
@@ -40,7 +42,7 @@ class ClientGenerator:
 
     def _nickelmenu_config(self) -> str:
         return (
-            "experimental:menu_main_15505_label:KoSync"
+            "experimental:menu_main_15505_label:KoSync\n"
             "menu_item :main    :Synchronise        :cmd_spawn          :quiet:/mnt/onboard/kosync_client"
         )
 
@@ -48,30 +50,48 @@ class ClientGenerator:
         client_path = Path(self._settings.client_path)
 
         if not client_path.exists():
-            raise ValueError(f"No client executable found at the set path: {client_path.name}")
-        
-        shutil.copyfile(client_path, Path(self.root_directory) / "mnt" / "onboard" / "kosync_client")
-        (Path(self.root_directory) / "mnt" / "onboard" / ".adds" / "nm" / "doc").write_text(self._nickelmenu_config())
+            raise ValueError(
+                f"No client executable found at the set path: {client_path.name}"
+            )
+
+        shutil.copyfile(
+            client_path, Path(self.root_directory) / "mnt" / "onboard" / "kosync_client"
+        )
+        (
+            Path(self.root_directory) / "mnt" / "onboard" / ".adds" / "nm" / "doc"
+        ).write_text(self._nickelmenu_config())
 
     def generate(self, token: str) -> Path:
-        config_path = Path(self.root_directory) / "mnt" / "onboard" / "kosync" / ".kosyncConfig.json"
+        config_path = (
+            Path(self.root_directory)
+            / "mnt"
+            / "onboard"
+            / "kosync"
+            / ".kosyncConfig.json"
+        )
 
         config_path.parent.mkdir(exist_ok=True)
-        
-        config_path.write_text(json.dumps({
-            "Token": token,
-            "BooksDirectory": "/mnt/onboard/kosync/",
-            "Endpoint": "https://books.guus.tech/"
-        }))
+
+        config_path.write_text(
+            json.dumps(
+                {
+                    "Token": token,
+                    "BooksDirectory": "/mnt/onboard/kosync/",
+                    "Endpoint": "https://books.guus.tech/",
+                }
+            )
+        )
 
         path = Path(self.root_directory) / "generated" / f"{token}.tgz"
         path.parent.mkdir(exist_ok=True)
 
         with tarfile.open(path, mode="w:gz") as tar_file:
             for subdir in ["usr", "mnt"]:
-                tar_file.add(Path(self.root_directory) / subdir)
-        
-        config_path.unlink()
-        
-        return path 
+                tar_file.add(
+                    name=Path(self.root_directory) / subdir,
+                    arcname=subdir,
+                )
 
+        config_path.unlink()
+
+        return path
--- backend/tests/test_client_generation.py
+++ backend/tests/test_client_generation.py
@@ -1,23 +1,75 @@
 import os
 from pathlib import Path
+import shutil
+import tarfile
+import tempfile
+from unittest import mock
+
+import pytest
+from pytest_mock import MockerFixture
 
 from kosync_backend.client_generator import ClientGenerator
 from kosync_backend.config import Settings
 
-def test_client_generation_fetches_all_resources() -> None:
+
+@pytest.fixture()
+def mock_nickelmenu_download(mocker: MockerFixture) -> mock.Mock:
+    kobo_root_path = Path(__file__).parent / "resources" / "nickelmenu_KoboRoot.tgz"
+
+    return mocker.patch(
+        f"{ClientGenerator.__module__}.urlretrieve",
+        return_value=(str(kobo_root_path.resolve()), None),
+    )
+
+
+def test_client_generation_fetches_all_resources(
+    mock_nickelmenu_download: mock.Mock,
+) -> None:
     with ClientGenerator(Settings()) as client_generator:
+        assert mock_nickelmenu_download.called
+
         root_path = Path(str(client_generator.root_directory))
 
-        assert set(os.listdir(root_path)) == {"usr",  "mnt"}
+        assert set(os.listdir(root_path)) == {"usr", "mnt"}
 
-        nm_config = Path(client_generator.root_directory) / "mnt" / "onboard" / ".adds" / "nm" / "doc"
+        nm_config = (
+            Path(client_generator.root_directory)
+            / "mnt"
+            / "onboard"
+            / ".adds"
+            / "nm"
+            / "doc"
+        )
 
         assert "KoSync" in nm_config.read_text()
-        assert (Path(client_generator.root_directory) / "mnt" / "onboard" / "kosync_client").exists()
+        assert (
+            Path(client_generator.root_directory) / "mnt" / "onboard" / "kosync_client"
+        ).exists()
 
 
-def test_client_generation_generates_a_client_archive() -> None:
-    with ClientGenerator(Settings()) as client_generator:
-        archive_path = client_generator.generate("foo")
+def test_client_generation_generates_a_client_archive(
+    mock_nickelmenu_download: mock.Mock,
+) -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with ClientGenerator(Settings()) as client_generator:
+            archive_path = client_generator.generate(token := "foo")
 
-        assert archive_path.exists()
+            assert archive_path.exists()
+            shutil.copy(archive_path, client_path := Path(temp_dir) / "KoboRoot.tgz")
+
+        assert client_path.exists()
+        with tarfile.open(client_path, "r:gz") as client_archive:
+            client_archive.extractall(path=client_path.parent, filter="tar")
+
+        assert (client_path.parent / "mnt" / "onboard" / "kosync_client").exists()
+        assert (
+            "KoSync"
+            in (
+                client_path.parent / "mnt" / "onboard" / ".adds" / "nm" / "doc"
+            ).read_text()
+        )
+        assert f'"Token": "foo"' in (
+            (
+                client_path.parent / "mnt" / "onboard" / "kosync" / ".kosyncConfig.json"
+            ).read_text()
+        )
